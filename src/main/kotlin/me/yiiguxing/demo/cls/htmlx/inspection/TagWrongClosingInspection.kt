8675f2ca38aa8800fa8383736deea5e3880fc5e7
Yii.Guxing <yii.guxing@gmail.com>
2021-12-03 00:55:26 +0800

    Annotator

---
+++ src/main/kotlin/me/yiiguxing/demo/cls/htmlx/inspection/TagWrongClosingInspection.kt
@@ -0,0 +1,24 @@
+package me.yiiguxing.demo.cls.htmlx.inspection
+
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.psi.PsiElement
+import me.yiiguxing.demo.cls.htmlx.psi.SimpleHTMLNotEmptyTag
+
+class TagWrongClosingInspection : Annotator {
+
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+
+        if (element is SimpleHTMLNotEmptyTag) {
+            val startTagName = element.startTagNameElement ?: return
+            val endTagName = element.endTagNameElement ?: return
+            if (startTagName.text != endTagName.text) {
+                holder.newAnnotation(HighlightSeverity.ERROR, "Tag has wrong closing tag")
+                    .range(startTagName)
+                    .create()
+            }
+        }
+    }
+
+}
